d8f28857766027340af60797fb961517bce71f32
Conway <e.conway@northeastern.edu>
2022-10-13 13:09:43 -0400

    typos

--- VisSatSatelliteStereo/WV3_Bounds.py
+++ VisSatSatelliteStereo/WV3_Bounds.py
@@ -15,6 +15,7 @@ def main(direct,Nview=10,rangex=2000,rangey=2000):
     nwlats = []
     selats = []
     selons = []
+    filenames = []
     ntffiles = []
     
     files = glob.glob(direct+'/*.NTF')
@@ -39,6 +40,7 @@ def main(direct,Nview=10,rangex=2000,rangey=2000):
         des_folder = os.path.join(file.split('.NTF')[0], order_id, subfolder, order_id)
         rpcfile = glob.glob(des_folder+'/*.XML')[0]
         rpcfiles.append(rpcfile)
+        
 
         with open(rpcfile,'r') as f:
             rpcdata = f.readlines()
@@ -56,33 +58,36 @@ def main(direct,Nview=10,rangex=2000,rangey=2000):
         nwlats.append(NWLAT)
         selats.append(SELAT)
         selons.append(SELON)    
-    
+        filenames.append(rpcfile)
+
     nwlons = np.array(nwlons,dtype=np.float64)
     nwlats = np.array(nwlats,dtype=np.float64)
     selons = np.array(selons,dtype=np.float64)
     selats = np.array(selats,dtype=np.float64)
+    filenames = np.array(filenames,dtype=np.object)
+
 
     max_lon = np.max(selons) ; min_lon = np.min(nwlons)
     max_lat = np.max(nwlats) ; min_lat = np.min(selats)
 
-    print(min_lon,max_lon)
-    print(min_lat,max_lat)
+    #print(min_lon,max_lon)
+    #print(min_lat,max_lat)
 
     pt1 = (max_lat,max_lon)
     pt2 = (max_lat,max_lon+1)
     pt3 = (max_lat+1,max_lon)
 
-    vert_dist = distance.distance(pt1,pt2).km
-    horz_dist = distance.distance(pt1,pt3).km
-    print(vert_dist,horz_dist)
+    vert_dist = distance.distance(pt1,pt3).km
+    horz_dist = distance.distance(pt1,pt2).km
+    #print(vert_dist,horz_dist)
 
     nres_x = rangex*1e-5 #degrees
     nres_y = nres_x *(vert_dist/horz_dist) # degrees
 
-    npix_x = rangey/0.31
-    npix_y = 2000*(vert_dist/horz_dist)/0.31
+    npix_x = nres_x/0.31
+    npix_y = npix_x*(vert_dist/horz_dist)
 
-    print(npix_x,npix_y)    
+    #print(npix_x,npix_y)    
 
     lon = np.arange(min_lon,max_lon,nres_x)
     lat = np.arange(min_lat,max_lat,nres_y)
@@ -101,18 +106,11 @@ def main(direct,Nview=10,rangex=2000,rangey=2000):
             idy = np.logical_and(nwlats>=lat_upper,selats<=lat_lower)
             idx = np.logical_and(nwlons<=lon_left,selons>=lon_right)
             idf = np.logical_and(idx,idy)
+            #if(np.sum(idf)>10):
+            #    print(np.sum(np.isfinite(idf)))
             tile_count[i,j] = np.sum(idf)
-
-            
-
-    for i in range(nlat-1):
-        lat_lower = lat[i]
-        lat_upper = lat[i+1]
-        for j in range(nlon-1):
-            lon_left = lon[j]
-            lon_right = lon[j+1]
             if(tile_count[i,j]>=Nview):
-                #print(lat_upper,lon_left)
+                #print(i,j,lat_upper,lon_left)
                 utm_point = utm.from_latlon(lat_upper,lon_left)
                 easting = utm_point[0]
                 northing = utm_point[1]
@@ -121,10 +119,13 @@ def main(direct,Nview=10,rangex=2000,rangey=2000):
                     hemi = 'N'
                 else:
                     hemi = 'S'
-                print(easting,northing,hemi,zone)
+                print(i,j,easting,northing,hemi,zone)
+
+            
+
 
                 
 if __name__ == '__main__':
     
     direct = '/3DReconstruction/3DReconstruction/JacksonvilleWV3'
-    main(direct,Nview=10,rangex=2000,rangey=2000)
+    main(direct,Nview=10,rangex=1000,rangey=1000)
